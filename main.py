2b20d64edcf3fc60e4c49b34adb2107bcd69c03a
embm4-ua <98742666+embm4-ua@users.noreply.github.com>
2022-06-27 00:41:28 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import subprocess, shutil
-
-def git_updater():
-    shutil.copyfile("me-pican-los-cocos/data/updater.py", "updater.py")
-    subprocess.Popen("powershell python updater.py", creationflags=subprocess.CREATE_NEW_CONSOLE)
-
-git_updater()
