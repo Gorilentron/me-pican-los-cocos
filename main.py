4d55e11429421b684a08c31e218f238399a0a4f0
embm4-ua <98742666+embm4-ua@users.noreply.github.com>
2022-07-24 12:48:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import subprocess
 init()
 
 def run(cmd):
-    return subprocess.run(["powershell", *cmd.split(' ')], capture_output=True)
+    txt = f"powershell {cmd}"
+    return subprocess.run(txt, capture_output=True)
 
-def git_update(new):
+def git_update():
     process = run("git reset --hard HEAD; git clean -d -f; git pull")
     print(str(process))
 
@@ -13,17 +14,9 @@ def git_update(new):
     elif "Already up to date" in str(process): print(Fore.LIGHTYELLOW_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTYELLOW_EX}] Already up to date" + Fore.RESET)
     else: print(Fore.LIGHTRED_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTRED_EX}] Not a git repository" + Fore.RESET)
 
-    with open("actual.txt", "w+") as file:
-        file.write(new)
-
 def git_check():
-    process = run("git rev-list --all --count")
-    new = int(str(process.stdout).replace("\\n","")[2:-1])
-
-    with open("actual.txt", "r") as file:
-        act = int(file.read())
-
-    if act != new: 
-        git_update(new)
+    # process = run("git pull --rebase")
+    # print(process.stderr)
+    git_update()
 
 git_check()
