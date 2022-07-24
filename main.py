db5b99c39d9935ecd110c7f873d569b01e84a7e4
embm4-ua <98742666+embm4-ua@users.noreply.github.com>
2022-07-24 13:11:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ def git_update():
     print(str(process))
 
     if 'changed' in str(process): print(Fore.LIGHTGREEN_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTGREEN_EX}] Updated" + Fore.RESET)
-    elif "Already up to date" in str(process): print(Fore.LIGHTYELLOW_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTYELLOW_EX}] Already up to date" + Fore.RESET)
     else: print(Fore.LIGHTRED_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTRED_EX}] Not a git repository" + Fore.RESET)
 
 def git_check():
-    # process = run("git pull --rebase")
-    # print(process.stderr)
-    git_update()
+    process = run("git pull")
+    print(f"{process.stdout} - {process.stderr}")
+
+    if "Already up to date" in str(process.stdout): 
+        print(Fore.LIGHTGREEN_EX + f"[{Fore.LIGHTWHITE_EX}·{Fore.LIGHTGREEN_EX}] Already up to date" + Fore.RESET)
+    else: git_update()
 
 git_check()
